3f06e20624fa849eb38c6acb71eaad2195adabb9
FraBif <francescobif@hotmail.it>
2023-06-21 16:25:26 +0200

    Created components empty

---
+++ boolflix/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<template>
+    <header>
+       <h1>
+        Boolflix
+       </h1>
+    </header>
+</template>
+
+<style scoped>
+  header{
+    background-color: black;
+    width: 100%;
+    height: 200px;
+  }
+
+  h1{
+    color: red;
+  }
+</style>
