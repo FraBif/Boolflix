2b6735dad206c5f9bdd6b877b0f9884fd44560af
FraBif <francescobif@hotmail.it>
2023-06-22 16:11:10 +0200

    Updated header and searchbar

--- boolflix/src/components/SearchBar.vue
+++ boolflix/src/components/SearchBar.vue
@@ -1,8 +1,25 @@
 <script>
 
+export default{
+    name:'SearchBar',
+    data(){
+        return{
+            searchedFilm: '',
+        }
+    }
+}
+
 </script>
 
 <template>
+    <div class="d-flex align-items-center">
+        <input type="text" name="" id="" placeholder="Films Searcher" class="py-1" v-model="searchedFilm" >
+        <button  @click="$emit('searched', searchedFilm)" type="button" class="btn btn-outline-primary mx-3">Find a film</button>
+
+
+
+    </div>
+
 
 </template>
 
